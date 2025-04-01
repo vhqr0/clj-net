01526819476d61d52d10744f44ba84784fe828ff
vhqr0 <zq_cmd@163.com>
2025-04-01 22:40:49 +0800

    update test

--- test/clj_net/inet/pcap_test.clj
+++ test/clj_net/inet/pcap_test.clj
@@ -5,14 +5,26 @@
             [clj-net.inet.pcap :as pcap])
   (:import [java.io InputStream]))
 
-(defn read-seq
+(defn bytes-seq
   [^InputStream r]
   (let [b (b/make 4096)
         n (.read r b 0 4096)]
     (when (pos? n)
       (lazy-seq
-       (cons (b/sub! b 0 n) (read-seq r))))))
+       (cons (b/sub! b 0 n) (bytes-seq r))))))
 
 (defn pcap-seq
   [r]
-  (->> (read-seq r) (sequence (pcap/->pcap-read-xf))))
+  (->> (bytes-seq r) (sequence (pcap/->pcap-read-xf))))
+
+(defn packet-seq
+  [r]
+  (->> (pcap-seq r) rest (map :data)))
+
+(defn parsed-packet-seq
+  [r]
+  (->> (packet-seq r) (map inet/parse-ether)))
+
+(defn read-pcap
+  [f]
+  (parsed-packet-seq (io/input-stream f)))
